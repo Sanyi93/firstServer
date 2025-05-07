fde107826f807e38f4abb00f797f8b31b13120d3
TMS <tms@bububububu.hu>
2025-05-07 15:33:18 -0400

    indexWithRequire file changed

--- CD220Labs/http_server/index-with-require.js
+++ CD220Labs/http_server/index-with-require.js
@@ -8,7 +8,24 @@ const today = require('./today');
 const requestListener = function (req, res) {
     res.writeHead(200); // Set the status code to 200 (OK)
     // Send the response with the current date from the 'today' module
-    res.end(`Hello, World! The date today is ${today.getDate()}`);
+
+    //getting the dateValue
+    let dateValue = today.getDate();
+
+    //Setting the greeting
+    let greeting = "No Time To Greet, Just Sleep"
+    if(dateValue.getHours() > 6 && dateValue.getHours() <= 12) {
+        greeting = "Good morning!";
+    } else if(dateValue.getHours() > 12 && dateValue.getHours() <= 18) {
+        greeting = "Good afternoon!"
+    } else if(dateValue.getHours() > 18 && dateValue.getHours() <= 21) {
+        greeting = "Good evening!"
+    } else if(dateValue.getHours() > 21 && dateValue.getHours() <= 24 ){
+        greeting = "Good night!"
+    }
+
+    //Sending the response with greeting
+        res.end(`Hello, ${greeting}! The date today is ${today.getDate()}`);
 };
 
 // Define the port number
